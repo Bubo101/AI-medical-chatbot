b35b6f4dde301dfe2d7477a0ca06ab2155854abd
Boden <boden.bradley@gmail.com>
2024-02-28 13:05:00 -0800

    updated port listening

--- chatbot_server/server.js
+++ chatbot_server/server.js
@@ -138,6 +138,7 @@ app.get('/user-metrics', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening on port ${port}`);
 });
+
